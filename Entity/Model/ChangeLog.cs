c1cb13d5306cce7a4ce948704c9489a635c8c739
JuanSerrato22 <serratojuan228@gmail.com>
2025-03-22 15:07:18 -0500

    Se creó la entidad ChangeLog

---
+++ Entity/Model/ChangeLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Model
+{
+    internal class ChangeLog
+    {
+        public int Id { get; set; }
+        public DateTime DeleteAt { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int IdTable { get; set; }
+        public int IdUser { get; set; }
+        public int IdPermission { get; set; }
+        public bool Action { get; set; }
+    }
+}
